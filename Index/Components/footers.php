d53e752152a97ee53571bfd7ecd4e70f35b54e85
NoirCipher <adedejideola14@gmail.com>
2026-01-24 11:08:38 -1100

    Add php files

---
+++ Index/Components/footers.php
@@ -0,0 +1,7 @@
+<footer>
+  About Us <br>
+  FAQs 
+  Sign up <br>
+  Sign in <br>
+  © 2026 Tutor Platform. All rights reserved.
+</footer>
---
+++ Index/Components/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="nav">
+  <h2 class="logo">Tutor</h2>
+    <a href="#">Home</a>
+    <a href="#">Tutors</a>
+    <a href="#">Subjects</a>
+    <a href="#" class="btn">Register</a>
+    <a href="#" class="btn">Become a Tutor</a>
+</div>
+</header>
